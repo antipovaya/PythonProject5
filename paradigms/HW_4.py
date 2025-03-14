b997d0947bb7ee4e320a01f9f3f0e64417a1f82b
antipovaya <antipovayana01@gmail.com>
2025-03-14 14:14:04 +0300

    парадигмы 4 Коэф. корреляции Пирсона

--- paradigms/HW_4.py
+++ paradigms/HW_4.py
@@ -23,10 +23,10 @@ def pearson_correlation(array_1, array_2):
     mean_x = sum(array_1) / n
     mean_y = sum(array_2) / n
 
-    variance_x = sum([(xi - mean_x) ** 2 for xi in array_1]) / float(len(array_1))
-    variance_y = sum([(yi - mean_y) ** 2 for yi in array_2]) / float(len(array_2))
+    variance_x = sum([(xi - mean_x) ** 2 for xi in array_1]) #/ float(len(array_1))
+    variance_y = sum([(yi - mean_y) ** 2 for yi in array_2]) #/ float(len(array_2))
 
-    covariance = sum([(xi - mean_x) * (yi - mean_y) for xi, yi in zip(array_1, array_2)]) / float(len(array_1))
+    covariance = sum([(xi - mean_x) * (yi - mean_y) for xi, yi in zip(array_1, array_2)]) #/ float(len(array_1))
     correlation = covariance / (math.sqrt(variance_x * variance_y))
 
     return correlation
