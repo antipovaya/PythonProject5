b52c37356114f26354758aefdc7d4b2ede9ad25f
antipovaya <antipovayana01@gmail.com>
2025-03-17 19:12:05 +0300

    степик задачки

---
+++ tasks/task08.py
@@ -0,0 +1,15 @@
+# for i in range(1, 4):
+#     for j in range(3, 5):
+#         print(i + j, end='')
+
+# counter = 0
+# for i in range(99, 102):
+#     temp = i
+#     while temp > 0:
+#         counter += 1
+#         temp //= 10
+#
+# print(counter)
+
+
+
