a70eaef357c52c652e80d9b496e8711cfdec461d
antipovaya <antipovayana01@gmail.com>
2025-04-15 17:35:07 +0300

    итоговая по реестру животных с фабрикой и подключением к sqlite

---
+++ tasks/test_model.py
@@ -0,0 +1,7 @@
+import sqlite3
+con = sqlite3.connect("metanit.db")
+cursor = con.cursor()
+# создаем таблицу people
+cursor.execute("""CREATE TABLE people (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, age INTEGER) """)
+
+con.close()
